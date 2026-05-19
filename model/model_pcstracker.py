801e5141531ddd501b6dcf9a94d8849cb3ce3567
Min Lin <126840566+MinLin2022@users.noreply.github.com>
2026-05-19 20:22:56 +0800

    Update model_pcstracker.py

--- model/model_pcstracker.py
+++ model/model_pcstracker.py
@@ -33,19 +33,6 @@ def sample_pos_embed(grid_size, embed_dim, coords):
     return sampled_pos_embed
 
 
-def bilinear_sampler(img, coords, mode="bilinear", mask=False):
-    H, W = img.shape[-2:]
-    xgrid, ygrid = coords.split([1, 1], dim=-1)
-    xgrid = 2 * xgrid / (W - 1) - 1
-    ygrid = 2 * ygrid / (H - 1) - 1
-    grid = torch.cat([xgrid, ygrid], dim=-1)
-    img = F.grid_sample(img, grid, align_corners=True)
-    if mask:
-        mask = (xgrid > -1) & (ygrid > -1) & (xgrid < 1) & (ygrid < 1)
-        return (img, mask.float())
-    return img
-
-
 class TrackerIterationModule(nn.Module):
 
     def __init__(
